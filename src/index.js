36eb294fea40b97f2b35ae1e2f41d0c638f8e893
KathrinShpigel <kathrin.shpigel.90@gmail.com>
2020-10-07 23:57:47 +0300

    feat: add human-readable-number solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
+  if (!number || number == 0) return 'zero';
+
+  const digits = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty'];
+  const tens = ['','ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+  let str = '';
+  let dig = 0;
+
   
+  if (number >= 100) {
+    dig = (number / 100) | 0;
+    str = `${ digits[dig] } hundred`;
+    number -= dig * 100;
+  }
+
+  if (number > 19) {
+    dig = (number / 10) | 0;
+		number -= dig * 10;
+    str = `${ str } ${ tens[dig] } ${ digits[number] }`;
+  } else {
+    str = `${ str } ${ digits[number] }`;
+  }
+
+  return str.trim();
 }
